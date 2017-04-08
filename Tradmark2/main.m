230423aebb67aa3d431787e013fe27a44c1f10c3
EXCELLENT2 <excellent2@EXCELLENT2s-Mac-mini.local>
2017-04-08 09:11:26 +0530

    Initial Commit

---
+++ Tradmark2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tradmark2
+//
+//  Created by EXCELLENT2 on 08/04/17.
+//  Copyright © 2017 EXCELLENT2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
