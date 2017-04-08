53039ec26edd16fe5efc2532e63a566f5e42c50d
EXCELLENT2 <excellent2@EXCELLENT2s-Mac-mini.local>
2017-04-08 12:48:44 +0530

    today

--- ProductDetailViewController.m
+++ ProductDetailViewController.m
@@ -46,7 +46,14 @@
 //    return 0;
 //}
 - (IBAction)btnMenu:(id)sender {
-   
+    UIViewController *vc = [self.storyboard instantiateViewControllerWithIdentifier:@"POPOVER"];
+       CATransition *transition = [CATransition animation];
+    transition.duration = 0.6;
+    transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    transition.type = kCATransitionMoveIn;
+    transition.subtype = kCATransitionFromLeft;
+    [vc.view.window.layer addAnimation:transition forKey:nil];
+    [self presentViewController:vc animated:YES completion:nil];
 }
 
 @end
